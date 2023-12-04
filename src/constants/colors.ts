411d6f997199e2f578d2c405bcbea50cbcd2548a
JosepDev23 <josepdev23@gmail.com>
2023-12-04 17:42:40 +0100

    add color constant

---
+++ src/constants/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+  lightMode: {
+    text: '#060E01',
+    background: '#F7FEF3',
+    primary: '#65F120',
+    secondary: '#86F8D0',
+    accent: '#56F5F2',
+  },
+  darkMode: {
+    text: '#F6FEF1',
+    background: '#060E01',
+    primary: '#53DD0E',
+    secondary: '#077851',
+    accent: '#0AA9A6',
+  },
+}
