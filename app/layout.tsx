e1366e895223fb7fbbca6f519b36c1d9cf2cf598
JosepDev23 <josepdev23@gmail.com>
2023-12-04 16:07:26 +0100

    create top bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import styles from './layout.module.css'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,12 +16,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="es">
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className={styles.nav_bar}></div>
+        <div className={styles.body}>{children}</div>
+      </body>
     </html>
   )
 }
